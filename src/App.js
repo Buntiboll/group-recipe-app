fa55b078ad6b53097809535bef7d66d0058cef2c
Buntiboll <43599829+Buntiboll@users.noreply.github.com>
2019-04-22 12:35:31 +0200

    Mattis kod

--- src/App.js
+++ src/App.js
@@ -45,3 +45,51 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+import React, { Component } from 'react';
+import './App.css';
+
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = "3847b22beb032b2d3af026eb77adea83";
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+        const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=8`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+    console.log(this.state.recipes);
+  }
+  componentDidMount = () => {
+    if(localStorage.getItem("recipes")){
+      const json = localStorage.getItem("recipes") || {};
+      const recipes = JSON.parse(json)||{};
+      this.setState({ recipes });
+    }
+    // Lägg till get recipe
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
