9c54db0c59d0530f1dc615eb5590e1579254aef3
Buntiboll <43599829+Buntiboll@users.noreply.github.com>
2019-04-29 09:18:43 +0200

    something
    vet ej

--- src/App.js
+++ src/App.js
@@ -4,56 +4,6 @@ import './App.css';
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
 
-const API_KEY = "22cc7acaddbec2295e010551a7178dfb";
-
-class App extends Component {
-  state = {
-    recipes: []
-  }
-  getRecipe = async (e) => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=8`);
-    
-    const data = await api_call.json();
-    
-    this.setState({ recipes: data.recipes });
-    console.log(this.state.recipes);
-  }
-
-  componentDidMount = () => {
-    const json = localStorage.getItem("recipes");
-    const recipes = JSON.parse(json);
-    this.setState({ recipes });
-  }
-
-  componentDidUpdate = () => {
-    const recipes = JSON.stringify(this.state.recipes);
-    localStorage.setItem("recipes", recipes);
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
-        </header>
-        <Form getRecipe={this.getRecipe} />
-        <Recipes recipes={this.state.recipes} />
-      </div>
-    );
-  }
-}
-
-export default App;
-
-
-
-import React, { Component } from 'react';
-import './App.css';
-
-import Form from "./components/Form";
-import Recipes from "./components/Recipes";
-
 const API_KEY = "3847b22beb032b2d3af026eb77adea83";
 
 class App extends Component {
@@ -63,7 +13,7 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-        const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=8`);
+        const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`);
 
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
